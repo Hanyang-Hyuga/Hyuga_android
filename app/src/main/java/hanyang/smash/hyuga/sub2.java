0298ca7a09907e4e1cace3e1df1a7fb885e588bd
윤 지우 <wldnthsus@icloud.com>
2016-01-25 19:58:38 +0900

    commit

---
+++ app/src/main/java/hanyang/smash/hyuga/sub2.java
@@ -0,0 +1,19 @@
+package hanyang.smash.hyuga;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by JEEWOOYOON on 2016. 1. 25..
+ */
+public class sub2 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View sub2 = inflater.inflate(R.layout.activity_sub_activity2, container, false);
+        return sub2;
+    }
+}
