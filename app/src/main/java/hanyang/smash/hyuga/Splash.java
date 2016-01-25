4cd87fb5b7e45590fa573197bb5a69f8d9a2d37a
Na Yun Hwan <kbk9288@gmail.com>
2016-01-25 20:06:04 +0900

    Test Commit

--- app/src/main/java/hanyang/smash/hyuga/Splash.java
+++ app/src/main/java/hanyang/smash/hyuga/Splash.java
@@ -24,7 +24,7 @@ public class Splash extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                Intent i = new Intent(getApplicationContext(), WebviewPage.class);
                 startActivity(i);
 
                 // close this activity
--- app/src/main/java/hanyang/smash/hyuga/TapPageAdpater.java
+++ app/src/main/java/hanyang/smash/hyuga/TapPageAdpater.java
@@ -1,34 +1,32 @@
-package hanyang.smash.hyuga;
-
-import android.app.FragmentManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentStatePagerAdapter;
-
-/**
- * Created by JEEWOOYOON on 2016. 1. 25..
- */
-public class TapPageAdpater extends FragmentStatePagerAdapter{
-    public TapPageAdpater (FragmentManager fm) {
-        super(fm);
-    }
-    @Override
-    public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                //Fragement for Android Tab
-                return new sub1();
-            case 1:
-                //Fragment for Ios Tab
-                return new sub2();
-            case 2:
-                //Fragment for Windows Tab
-                return new sub3();
-        }
-        return null;
-    }
-
-    @Override
-    public int getCount() {
-        return 3;
-    }
-}
+//package hanyang.smash.hyuga;
+//
+//import android.app.FragmentManager;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentStatePagerAdapter;
+//
+///**
+// * Created by JEEWOOYOON on 2016. 1. 25..
+// */
+//public class TapPageAdpater extends FragmentStatePagerAdapter{
+//
+//    @Override
+//    public Fragment getItem(int position) {
+//        switch (position) {
+//            case 0:
+//                //Fragement for Android Tab
+//                return new sub1();
+//            case 1:
+//                //Fragment for Ios Tab
+//                return new sub2();
+//            case 2:
+//                //Fragment for Windows Tab
+//                return new sub3();
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return 3;
+//    }
+//}
